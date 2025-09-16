c345669ae3d5e18c0d3de703528110fedf3da402
Kamini <kamini14112004@gmail.com>
2025-09-16 22:23:55 +0530

    corrected user creation

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -19,7 +19,7 @@ export const clerkWebhooks = async(req,res)=>{
             case 'user.created':{
                 const userData = {
                     _id: data.id,
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name:data.first_name + "" + data.last_name,
                     imageUrl: data.image_url,
 
@@ -34,7 +34,7 @@ export const clerkWebhooks = async(req,res)=>{
             case 'user.updated':{
                 const userData = {
                     _id:data.id,
-                    email:data.email_address[0].email_address,
+                    email:data.email_addresses[0].email_address,
                     name:data.first_name+" "+ data.last_name,
                     imageUrl: data.image_url,
                 }
